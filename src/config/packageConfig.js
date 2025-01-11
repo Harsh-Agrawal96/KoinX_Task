6dc06377f0ae5325a9d4d02a1c8bc910f71607e9
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2025-01-11 16:52:26 +0530

    configuration for deploy on vercel

--- src/config/packageConfig.js
+++ src/config/packageConfig.js
@@ -1,7 +1,17 @@
 import bodyParser from "body-parser";
+import cors from "cors"
 
 
 let libraryConfig = (app) => {
+
+    const corsConfig = {
+        origin : "*",
+        credential: true,
+        methods : ["GET", "POST", "PUT", "DELETE"],
+    };
+
+    app.use(cors(corsConfig));
+    app.options("", cors(corsConfig));
     
     // body parser
     app.use(bodyParser.json());
